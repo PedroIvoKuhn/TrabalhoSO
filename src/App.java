351b03c0b590c729de1b187b48703e19f6889581
Pedro Ivo Kuhn <kuhnpedroivo@gmail.com>
2024-08-08 16:45:06 -0300

    encerrando depois de um tempo(ver bug dos karts)

--- src/App.java
+++ src/App.java
@@ -1,9 +1,9 @@
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Random;
+import java.util.List;
+import java.util.ArrayList;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
-import java.util.concurrent.TimeUnit;
+import java.util.concurrent.Future;
 
 import entities.Competidor;
 import entities.Kartodromo;
@@ -12,6 +12,7 @@ public class App {
     public static void main(String[] args) throws Exception {
         Kartodromo kartodromo = new Kartodromo(10, 10);
         ExecutorService executor = Executors.newCachedThreadPool();
+        List<Future<?>> proximos = new ArrayList<>();
         Random random = new Random();
 
         // Gerando competidores para o kartodromo
@@ -30,16 +31,31 @@ public class App {
         long startTime = System.currentTimeMillis();
 
         while (System.currentTimeMillis() - startTime < 16000) {
-            kartodromo.vaoCorrer(random.nextInt(12, 16), executor);
+            kartodromo.vaoCorrer(random.nextInt(12, 16), executor, proximos);
 
             Thread.sleep(1000); //Tempo ate a proxima chegada de pessoas 
         }
 
         executor.shutdown();
-        boolean terminated = executor.awaitTermination(0, TimeUnit.SECONDS);
-        if (!terminated) {
+        /*
+         * 
+         boolean terminated = executor.awaitTermination(0, TimeUnit.SECONDS);
+         if (!terminated) {
             System.out.println("Algumas tarefas não foram concluídas a tempo.");
         }
+        
+        */
+        for (Future<?> proximo : proximos) {
+            if (!proximo.isDone()) {
+                try {
+                    proximo.cancel(true);  
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        System.out.print("\033[H\033[2J");  
+        System.out.flush();
         kartodromo.relatorio();
     }
 }
